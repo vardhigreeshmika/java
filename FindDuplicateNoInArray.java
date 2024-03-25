80f1b6f4a7332496a3ef675524aca32a37b651c1
vardhigreeshmika <134677797+vardhigreeshmika@users.noreply.github.com>
2024-03-25 16:23:37 +0530

    Add files via upload

---
+++ FindDuplicateNoInArray.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindDuplicateNoInArray {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int x = Math.abs(nums[i]);
+            if (nums[x - 1] < 0) {
+                ans.add(x);
+            }
+            nums[x - 1] *= -1;
+        }
+        return ans;
+    }
+}
+    
+
